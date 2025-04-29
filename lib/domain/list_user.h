465bb317d6c00ab8b47dad1866f3bc8c9aada8a2
xirzo <easttem@gmail.com>
2025-04-29 17:34:08 +0300

    feat: list_user

---
+++ lib/domain/list_user.h
@@ -0,0 +1,14 @@
+#ifndef LIST_USER_H
+#define LIST_USER_H
+
+#include <cstdint>
+
+struct ListUser
+{
+    int64_t list_user_id;
+    int64_t list_id;
+    int64_t user_id;
+    int64_t list_user_order;
+};
+
+#endif  // !LIST_USER_H
