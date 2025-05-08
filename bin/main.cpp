c691d8139a8784555f9cc305188eb8a239574841
xirzo <easttem@gmail.com>
2025-05-08 13:09:00 +0300

    *feat: port set from env

--- bin/main.cpp
+++ bin/main.cpp
@@ -2,19 +2,24 @@
 
 #include "server.h"
 #include "sqlite_repository.h"
+#include <charconv>
+#include <cstdlib>
 #include <exception>
+#include <fstream>
+#include <iostream>
 #include <nlohmann/json.hpp>
+#include <stdexcept>
 
-constexpr std::size_t kPort = 5000;
 constexpr char kDbPath[] = "data.db3";
 constexpr char kUsersFile[] = "users.json";
 
 User fromJson(const nlohmann::json &json) {
-  return User(json.value("user_id", -1), json.at("telegram_id").get<int64_t>(),
-              json.at("first_name").get<std::string>(),
-              json.at("surname").get<std::string>(),
-              json.at("second_name").get<std::string>(),
-              json.at("admin").get<bool>());
+  return User(json.value("user_id", -1),
+              json.value("telegram_id", static_cast<int64_t>(-1)),
+              json.value("first_name", std::string()),
+              json.value("surname", std::string()),
+              json.value("second_name", std::string()),
+              json.value("admin", false));
 }
 
 std::vector<User> loadUsersFromFile(const char *file_path) {
@@ -35,6 +40,27 @@ std::vector<User> loadUsersFromFile(const char *file_path) {
 }
 
 int main() {
+  const char *port_env = std::getenv("port");
+  std::size_t port = 5000;
+
+  if (!port_env) {
+    std::cerr << "Env 'port' is not set, using default port: "
+              << port << std::endl;
+  } else {
+    std::string kPortStr(port_env);
+    if (kPortStr.empty()) {
+      std::cerr << "port environment variable is empty, using default port: "
+                << port << std::endl;
+    } else {
+      auto [ptr, ec] = std::from_chars(kPortStr.data(),
+                                       kPortStr.data() + kPortStr.size(), port);
+      if (ec != std::errc()) {
+        std::cerr
+            << "Error converting port string to number, using default port: "
+            << port << std::endl;
+      }
+    }
+  }
 
   try {
     const std::vector<User> kUsers = loadUsersFromFile(kUsersFile);
@@ -48,13 +74,15 @@ int main() {
     auto init_result = sqlite_repo->init();
 
     if (!init_result) {
-      std::cerr << init_result.error() << std::endl;
+      std::cerr << "Database initialization failed: " << init_result.error()
+                << std::endl;
       return 1;
     }
 
-    Server server(kPort, sqlite_repo);
+    Server server(port, sqlite_repo);
     return 0;
   } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
+    std::cerr << "Unhandled exception: " << e.what() << std::endl;
+    return 1;
   }
 }
