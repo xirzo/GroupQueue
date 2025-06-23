6b9809ca6eaab3e600e4dc524082b0b871ef0ebd
xirzo <easttem@gmail.com>
2025-06-23 19:47:02 +0300

    feat: libevent

--- bin/main.c
+++ bin/main.c
@@ -1,5 +1,16 @@
 #include "bool.h"
 #include "db.h"
+#include <event2/http.h>
+#include <event2/http_struct.h>
+#include <evhttp.h>
+#include <logger.h>
+
+#define IP   "127.0.0.1"
+#define PORT 5000
+
+void callback(struct evhttp_request *req, void *ctx) {
+    LOG_INFO("Callback!");
+}
 
 int main(void) {
     const char *conninfo =
@@ -9,24 +20,19 @@ int main(void) {
         return 1;
     }
 
-    GQaddUser((GQuser){
-        .telegram_id = 618214141,
-        .first_name = "Aleks",
-        .surname = "Lol",
-        .last_name = "Kekovich",
-        .is_admin = gqtrue,
-    });
+    struct event_base *base = event_base_new();
+    struct evhttp     *server = evhttp_new(base);
 
-    GQaddUser((GQuser){
-        .telegram_id = 618211245,
-        .first_name = "Bebr",
-        .surname = "Kek",
-        .last_name = "Lolovich",
-        .is_admin = gqfalse,
-    });
+    evhttp_bind_socket(server, IP, PORT);
+    evhttp_set_cb(server, "/list", callback, NULL);
 
-    GQaddList((GQlist){ .name = "Another List 1" });
-    GQaddList((GQlist){ .name = "Another List 2" });
+    // a evhttp_set_gencb(); this callback will be invoked if no other callbacks
+    // have been registered for a given URI.
+
+    event_base_dispatch(base);
+
+    evhttp_free(server);
+    event_base_free(base);
 
     GQdestroyDB();
     return 0;
@@ -34,3 +40,4 @@ int main(void) {
 
 // TODO: load initial state with toml files
 // TODO: remove list
+// TODO: http server for serving db methods
